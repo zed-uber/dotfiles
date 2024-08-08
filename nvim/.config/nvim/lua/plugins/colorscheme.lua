b958eb327e2885b1a605966b751c46370bb71499
zed-uber <erik.anders@gmail.com>
2024-08-08 16:33:45 -0500

    neovim lazygit muckabout

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  { "catppuccin" },
+
+  {
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
